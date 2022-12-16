b2ec1344f448374e1c01e2d4c1905bb90b2f3af0
mlasprilla <mlasprilla@ibm.com>
2022-12-16 12:21:18 -0500

    3

---
+++ ssp-env.sh
@@ -0,0 +1,11 @@
+export CM_SYS_PASS=mypasswOrd1!
+export CM_ADMIN_PASSWORD=mypasswOrd1!
+export CM_CERTSTORE_PASSWORD=mypasswOrd1!
+export CM_CERTENCRYPT_PASSWORD=mypasswOrd1!
+export CM_CUSTOMCERT_PASSWORD=mypasswOrd1!
+export ENGINE_SYS_PASS=mypasswOrd1!
+export ENGINE_CERTSTORE_PASSWORD=mypasswOrd1!
+export ENGINE_CERTENCRYPT_PASSWORD=mypasswOrd1!
+export ENGINE_CUSTOMCERT_PASSWORD=mypasswOrd1!
+export RWX_STORAGECLASS=ibmc-file-silver
+export NS=ssp
